b2bad8fd7bf4ee7935bf8b1ed3e9f53fc76c3488
zhouzhenpan <zhouzhenpan@corp.netease.com>
2024-06-27 17:05:30 +0800

    feat: pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     host: '0.0.0.0'
   },
-  plugins: [react(), UnoCSS()],
+  plugins: [
+    react(), 
+    UnoCSS(), 
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
 })
